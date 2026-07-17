22e72aa087eaba7f4db5426b6e2216bbab53a358
Venu Sai Madisetti <venusaim23@gmail.com>
2026-07-17 15:08:06 +0530

    Fixed harbor config. (#1)
    * add idea in gitignore.
    
    * pin docker image and delete leaked solution.
    
    * change artifacts to array.
    
    * fix tests path.
    
    * write actual tests.
    
    * update clear instructions.
    
    * logs.

--- tests/test.sh
+++ tests/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # pytest is baked into the environment image (environment/Dockerfile).
-pytest /tests/test_outputs.py -rA
+mkdir -p /logs/verifier
+
+pytest /tests/test_outputs.py -rA --ctrf=/logs/verifier/ctrf.json
 
 if [ $? -eq 0 ]; then
-  echo 1 > /app/reward.txt
+  echo 1 > /logs/verifier/reward.txt
 else
-  echo 0 > /app/reward.txt
+  echo 0 > /logs/verifier/reward.txt
 fi
